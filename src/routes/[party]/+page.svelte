971b85fab64013ea3466ae92bf12d8fe86fe87d1
Zeu Capua <coding@zeu.dev>
2024-01-03 15:38:33 -0800

    working alpha 2

---
+++ src/routes/[party]/+page.svelte
@@ -0,0 +1,167 @@
+<script lang="ts">
+  import PartySocket from "partysocket";
+  import { dev } from "$app/environment";
+  import { page } from "$app/stores";
+  import { onMount } from "svelte";
+
+  type Player = {
+    conn_id: string,
+    name: string,
+    score: number,
+    is_ready: boolean,
+  };
+  
+  /* VARIABLES */
+  // current universal user details
+  let conn_id = $state("");
+  let name = $state((Math.random() + 1).toString(36).substring(7)); // initial name
+
+  // party state 
+  const party_id = $page.params.party;
+  let players : Player[] = $state([]);
+  let game_state : "lobby" | "running" | "ending" = $state("lobby");
+
+  // lobby
+  let is_ready = $state(false);
+
+  // running
+  let user_string = $state("");
+  let target_string = $state("");  
+  let progress = $derived(
+    (target_string.length !== 0) ? 
+    (user_string.length / target_string.length) * 100
+    : 0
+  );
+
+  // change or reset variables based on game state
+  $effect(() => {
+    switch (game_state) {
+      case "lobby": {
+        is_ready = false;
+        break;
+      }
+      case "running": {
+        if (progress === 100) {
+          let num_incorrect = 0;
+          for (let i = 0; i < target_string.length; i++) {
+            num_incorrect += (user_string[i] !== target_string[i]) ? 1 : 0;
+          }
+          socket.send(JSON.stringify({
+            type: "finished",
+            values: {
+              result: ((user_string.length - num_incorrect) / target_string.length) * 100
+            }
+          }));   
+        }
+        break;
+      }
+      case "ending": {
+        is_ready = false;
+        target_string = "";
+        user_string = "";
+        break;
+      }
+    }
+  });
+
+
+  /* SOCKET */
+  const socket = new PartySocket({
+    host: dev ? "localhost:1999" : `https://typer99-app-party.zeucapua.partykit.dev/party/${party_id}`,
+    room: party_id,
+  });
+
+  /* FUNCTIONS */
+  // lobby
+  function updateName() {
+    if (socket) {
+      const data = JSON.stringify({
+        type: "updateName",
+        values: { name }
+      });
+
+      socket.send(data);
+    }
+  }
+  
+  // lobby + ending
+  function toggleReady() {
+    if (socket) {
+      const data = JSON.stringify({
+        type: "ready",
+        values: { is_ready }
+      });
+      socket.send(data);
+    }
+  }
+  
+  /* PARTYKIT BROADCAST LISTENER */
+  socket.addEventListener("message", async (event) => {
+    const data = JSON.parse(event.data);
+
+    switch (data.type) {
+      case "connected": {
+        conn_id = data.values.conn_id;
+        break;
+      }
+      case "mirror": {
+        players = data.values.players;
+        game_state = data.values.party_state;
+        target_string = data.values.target_string ?? "";
+        break;
+      }
+    }
+  });
+
+  onMount(() => {
+    updateName();
+  });
+</script>
+
+<p>Party #{party_id} ({conn_id})</p>
+{#if game_state === "lobby"}
+
+  <div>
+    <input bind:value={name} type="text" placeholder="Joe" />
+    <button onclick={updateName}>Update</button>
+    <label for="ready">
+      <input name="ready" type="checkbox" bind:checked={is_ready} onchange={toggleReady} />
+      Ready{is_ready ? "!" : "?"} 
+    </label>
+  </div>
+
+  Others:
+  {#each players as player : Player}
+    <!-- display other players' state -->
+    {#if player.conn_id !== conn_id}
+      <div>
+        <p>{player.name} | {player.is_ready}</p>
+      </div>
+    {/if}
+  {/each}
+
+{:else if game_state === "running"}
+  <h1>Game</h1>
+  <p>
+  {#each target_string as letter, i}
+    {#if (user_string.length === 0) || (i > user_string.length)}
+      <span style:color="gray">{letter}</span>
+    {:else if (letter === user_string[i])}
+      <span style:color="green">{letter}</span>
+    {:else}
+      <span style:color="red">{letter}</span> 
+    {/if}
+  {/each}
+  </p>
+  <input type="text" bind:value={user_string} disabled={progress === 100}/>
+  <progress value={progress} max={100} />
+{:else if game_state === "ending"}
+  <h1>Leaderboard</h1>
+  {#each players as player : Player}
+    <p>{player.name}: {player.score}</p>
+  {/each}
+  <label for="ready">
+    <input name="ready" type="checkbox" bind:checked={is_ready} onchange={toggleReady} />
+    {is_ready ? "Waiting for others" : "Back to Lobby?"} 
+  </label>
+{/if}
