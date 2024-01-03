7b48c5feb456287b56a23d93b47dfd9869ca440b
Zeu Capua <coding@zeu.dev>
2024-01-02 23:33:04 -0800

    first pass

--- party/index.ts
+++ party/index.ts
@@ -1,9 +1,11 @@
+import { faker } from "@faker-js/faker";
 import type * as Party from "partykit/server";
 
 type Player = {
   conn_id: string,
   name: string,
-  score: number
+  score: number,
+  is_ready: boolean,
 };
 
 export default class Server implements Party.Server {
@@ -14,6 +16,7 @@ export default class Server implements Party.Server {
   party_state : "lobby" | "running" | "ending" = "lobby";
   target_string = "";
   players : Player[] = [];
+  finished : { id: string, result: number }[] = [];
 
   onConnect(conn: Party.Connection, ctx: Party.ConnectionContext) {
     // A websocket just connected!
@@ -23,42 +26,160 @@ export default class Server implements Party.Server {
       room: ${this.party.id}
       url: ${new URL(ctx.request.url).pathname}`
     );
+
+    // create a player
+    this.players.push({
+      conn_id: conn.id,
+      name: "",
+      score: 0,
+      is_ready: false
+    });
+
+    conn.send(JSON.stringify({
+      type: "connected",
+      values: { conn_id: conn.id }
+    }));
+    
+    this.mirror();
   }
 
   onMessage(message: string, sender: Party.Connection) {
+    // parse JSON message
+    const { type, values } = JSON.parse(message);
+
     // check the party state (in lobby, in game, or game ended)
     if (this.party_state === "lobby") {
-      switch (message) {
+      switch (type) {
         case "ready": {
-          this.party_state = "running"
+          let num_ready = 0;
+
+          // update player ready
+          this.players.map((p) => {
+            if (p.conn_id === sender.id) { p.is_ready = values.is_ready };
+            if (p.is_ready) { num_ready += 1; }
+          });
+          this.mirror();
+
+          // TODO: refactor to show starting and allow unready to cancel
+          if (num_ready === this.players.length) {
+            setTimeout(() => {
+              this.party.broadcast(JSON.stringify({ type: "startGame" })); 
+              this.party_state = "running";
+              this.target_string = faker.word.words({ count: 5 });
+              this.finished = [];
+              this.mirror();
+            }, 5000);
+          }
+
+          break;
+        }
+        case "updateName": {
+          // update player name
+          this.players.map((p) => {
+            if (p.conn_id === sender.id) p.name = values.name;
+          });
+
+          this.mirror();
           break;
         }
       }
     }
 
     else if (this.party_state === "running") {
-      switch (message) {
+      switch (type) {
+        
+        // when a player is finished
+        case "finished": {
+          const result =  values.result; 
+          this.finished.push({ id: sender.id, result });
 
-        // sent by a player who won the game first
-        case "winner": {
-          this.party_state = "ending";
-          this.players.map((p) => {
-            // give player a score
-            if (p.conn_id === sender.id) { p.score += 1; }
-          });
-          break;
-        } 
+          if (this.finished.length === this.players.length) {
+            this.party_state = "ending";
+            for (let i = 0; i < this.finished.length; i++) {
+              const results = this.finished[i];
+              this.players.map((p) => {
+                // unready 
+                p.is_ready = false;
+
+                // calculate bonus 
+                if (results.id === p.conn_id) {
+                  p.score = results.result + (
+                    i === 0 ? 3
+                    : i === 1 ? 2
+                    : i === 2 ? 1 
+                    : 0
+                  )
+                }
+              });
+            }
+            this.party.broadcast(JSON.stringify({
+              type: "ending"
+            }));
+          }
+        }
+
       }
     }
 
     else if (this.party_state === "ending") {
+      // what to do in ending screen?
+      // emoji? chat?
+      switch (message) {
+        case "mirror": {
+          this.mirror();
+          break;
+        }
+        case "reset": {
+          let num_ready = 0;
 
+          // update player ready
+          this.players.map((p) => {
+            if (p.conn_id === sender.id) { p.is_ready = values.is_ready };
+            if (p.is_ready) { num_ready += 1; }
+          });
+
+          if (num_ready >= this.players.length) {
+            this.players.map((p) => {
+              p.score = 0;
+              p.is_ready = false;
+            });
+            this.party.broadcast(JSON.stringify({ type: "reset" }));
+          }
+        }
+      }
     }
   }
 
   // when a connection leaves, remove a Player associated
   onClose(connection: Party.Connection<unknown>): void | Promise<void> {
      this.players = this.players.filter((p) => p.conn_id !== connection.id);
+  }
+
+  mirror() {
+    const data = { type: "mirror", values: {} };
+    switch (this.party_state) {
+      case "lobby": {
+        data.values = {
+          players: this.players
+        }
+        break;
+      }
+      case "running": {
+        data.values = {
+          players: this.players,
+          target_string: this.target_string
+        }
+        break;
+      }
+      case "ending": {
+        data.values = {
+          players: this.players
+        }
+        break;
+      }
+    }
+
+    this.party.broadcast(JSON.stringify(data));
   }
 }
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  let party_id : string = $state("");
+</script>
+
+<h1>typer99</h1>
+<p>Join a party</p>
+<input bind:value={party_id} type="text" maxlength="4" />
+<a href={`/${party_id}`}>Enter</a>
